1b43263bc78c9eda94d4b0f2432bb292c4c2dd98
OldXiaHub <799532619@qq.com>
2019-08-01 21:19:48 +0800

    1

--- src/main/java/org/taru/api/entity/Role.java
+++ src/main/java/org/taru/api/entity/Role.java
@@ -1,37 +1,87 @@
 package org.taru.api.entity;
 
+
 public class Role {
-    private String roleId;
-    private String roleName;
-    private String roleStatus;
 
-    public Role(String roleId, String roleName, String roleStatus) {
-        this.roleId = roleId;
-        this.roleName = roleName;
-        this.roleStatus = roleStatus;
-    }
+  private long roleId;
+  private String roleName;
+  private String roleRemark;
+  private String create_By;
+  private java.sql.Timestamp createTime;
+  private String lastUpdateBy;
+  private java.sql.Timestamp lastUpdateTime;
+  private String delFlag;
 
-    public String getRoleId() {
-        return roleId;
-    }
 
-    public void setRoleId(String roleId) {
-        this.roleId = roleId;
-    }
+  public long getRoleId() {
+    return roleId;
+  }
 
-    public String getRoleName() {
-        return roleName;
-    }
+  public void setRoleId(long roleId) {
+    this.roleId = roleId;
+  }
 
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
 
-    public String getRoleStatus() {
-        return roleStatus;
-    }
+  public String getRoleName() {
+    return roleName;
+  }
 
-    public void setRoleStatus(String roleStatus) {
-        this.roleStatus = roleStatus;
-    }
+  public void setRoleName(String roleName) {
+    this.roleName = roleName;
+  }
+
+
+  public String getRoleRemark() {
+    return roleRemark;
+  }
+
+  public void setRoleRemark(String roleRemark) {
+    this.roleRemark = roleRemark;
+  }
+
+
+  public String getCreate_By() {
+    return create_By;
+  }
+
+  public void setCreate_By(String create_By) {
+    this.create_By = create_By;
+  }
+
+
+  public java.sql.Timestamp getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(java.sql.Timestamp createTime) {
+    this.createTime = createTime;
+  }
+
+
+  public String getLastUpdateBy() {
+    return lastUpdateBy;
+  }
+
+  public void setLastUpdateBy(String lastUpdateBy) {
+    this.lastUpdateBy = lastUpdateBy;
+  }
+
+
+  public java.sql.Timestamp getLastUpdateTime() {
+    return lastUpdateTime;
+  }
+
+  public void setLastUpdateTime(java.sql.Timestamp lastUpdateTime) {
+    this.lastUpdateTime = lastUpdateTime;
+  }
+
+
+  public String getDelFlag() {
+    return delFlag;
+  }
+
+  public void setDelFlag(String delFlag) {
+    this.delFlag = delFlag;
+  }
+
 }
--- src/main/java/org/taru/api/entity/User.java
+++ src/main/java/org/taru/api/entity/User.java
@@ -1,58 +1,117 @@
 package org.taru.api.entity;
 
+
 public class User {
-    private String userName;
-    private String userPassword;
-    private String userTelPhone;
-    private String userEmail;
-    private String userSex;
-    private String userStatus;
 
-    public String getUserName() {
-        return userName;
-    }
+  private long userId;
+  private String userName;
+  private String userPassword;
+  private String mobile;
+  private String userEmail;
+  private String userStatus;
+  private String createBy;
+  private java.sql.Timestamp createTime;
+  private String lastUpdateBy;
+  private String delFlag;
+  private java.sql.Timestamp lastUpdateTime;
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
 
-    public String getUserPassword() {
-        return userPassword;
-    }
+  public long getUserId() {
+    return userId;
+  }
 
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
+  public void setUserId(long userId) {
+    this.userId = userId;
+  }
 
-    public String getUserTelPhone() {
-        return userTelPhone;
-    }
 
-    public void setUserTelPhone(String userTelPhone) {
-        this.userTelPhone = userTelPhone;
-    }
+  public String getUserName() {
+    return userName;
+  }
 
-    public String getUserEmail() {
-        return userEmail;
-    }
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
 
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
 
-    public String getUserSex() {
-        return userSex;
-    }
+  public String getUserPassword() {
+    return userPassword;
+  }
 
-    public void setUserSex(String userSex) {
-        this.userSex = userSex;
-    }
+  public void setUserPassword(String userPassword) {
+    this.userPassword = userPassword;
+  }
 
-    public String getUserStatus() {
-        return userStatus;
-    }
 
-    public void setUserStatus(String userStatus) {
-        this.userStatus = userStatus;
-    }
+  public String getMobile() {
+    return mobile;
+  }
+
+  public void setMobile(String mobile) {
+    this.mobile = mobile;
+  }
+
+
+  public String getUserEmail() {
+    return userEmail;
+  }
+
+  public void setUserEmail(String userEmail) {
+    this.userEmail = userEmail;
+  }
+
+
+  public String getUserStatus() {
+    return userStatus;
+  }
+
+  public void setUserStatus(String userStatus) {
+    this.userStatus = userStatus;
+  }
+
+
+  public String getCreateBy() {
+    return createBy;
+  }
+
+  public void setCreateBy(String createBy) {
+    this.createBy = createBy;
+  }
+
+
+  public java.sql.Timestamp getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(java.sql.Timestamp createTime) {
+    this.createTime = createTime;
+  }
+
+
+  public String getLastUpdateBy() {
+    return lastUpdateBy;
+  }
+
+  public void setLastUpdateBy(String lastUpdateBy) {
+    this.lastUpdateBy = lastUpdateBy;
+  }
+
+
+  public String getDelFlag() {
+    return delFlag;
+  }
+
+  public void setDelFlag(String delFlag) {
+    this.delFlag = delFlag;
+  }
+
+
+  public java.sql.Timestamp getLastUpdateTime() {
+    return lastUpdateTime;
+  }
+
+  public void setLastUpdateTime(java.sql.Timestamp lastUpdateTime) {
+    this.lastUpdateTime = lastUpdateTime;
+  }
+
 }
