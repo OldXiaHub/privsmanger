a39e27d259fff5f241d842435e80454ba7ea237a
OldXiaHub <799532619@qq.com>
2019-08-13 12:23:33 +0800

    Default Changelist

---
+++ src/main/java/org/taru/api/entity/Power.java
@@ -0,0 +1,69 @@
+package org.taru.api.entity;
+
+
+public class Power {
+
+  private String powerId;
+  private String powerName;
+  private String powerSonId;
+  private String powerFather;
+  private String powerStatus;
+  private String powerGrade;
+  private String powerUrl;
+
+  public String getPowerId() {
+    return powerId;
+  }
+
+  public void setPowerId(String powerId) {
+    this.powerId = powerId;
+  }
+
+  public String getPowerName() {
+    return powerName;
+  }
+
+  public void setPowerName(String powerName) {
+    this.powerName = powerName;
+  }
+
+  public String getPowerSonId() {
+    return powerSonId;
+  }
+
+  public void setPowerSonId(String powerSonId) {
+    this.powerSonId = powerSonId;
+  }
+
+  public String getPowerFather() {
+    return powerFather;
+  }
+
+  public void setPowerFather(String powerFather) {
+    this.powerFather = powerFather;
+  }
+
+  public String getPowerStatus() {
+    return powerStatus;
+  }
+
+  public void setPowerStatus(String powerStatus) {
+    this.powerStatus = powerStatus;
+  }
+
+  public String getPowerGrade() {
+    return powerGrade;
+  }
+
+  public void setPowerGrade(String powerGrade) {
+    this.powerGrade = powerGrade;
+  }
+
+  public String getPowerUrl() {
+    return powerUrl;
+  }
+
+  public void setPowerUrl(String powerUrl) {
+    this.powerUrl = powerUrl;
+  }
+}
